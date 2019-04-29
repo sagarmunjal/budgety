8fc9880bac116e4743c337fd514ae258076baa92
Sagar <sagar@Sagars-MacBook-Air-2.local>
2019-04-29 21:21:31 +0530

    modular structure

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+var budgetController = (function(){
+    var x = 23;
+    var add = function(a){
+        return x + a;
+    }
 
+    return {
+        publicTest : function (b){
+            return add(b);
+        }
+    }
+})(); 
+
+var uiController = (function(){
+
+})
+
+var controller = (function(budgetCtrl,UIctrl){
+    var z = budgetCtrl.publicTest(5);
+    return {
+        anotherPublic: function(){
+            console.log(z)
+        }
+    }
+})(budgetController,uiController)
+
+controller.anotherPublic();
