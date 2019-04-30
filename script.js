a474f07392f81b02a0393e36d91f9bbf2a3d8633
Sagar <sagar@Sagars-MacBook-Air-2.local>
2019-04-30 14:27:54 +0530

    ui controller updated with get input function

--- script.js
+++ script.js
@@ -19,20 +19,25 @@ var uiController = (function(){
 
     return {
         getInput : function(){
-           var type = document.querySelector('.add__type').value;
+            return {
+                type : document.querySelector('.add__type').value,
+                description : document.querySelector('.add__description').value,
+                value : document.querySelector('.add__value').value
+            }
         }
     }
     
-})
+})();
 
 var controller = (function(budgetCtrl,UIctrl){
     var ctrlAddItem = function(){
         // 1. Get the field input data
+        var input = UIctrl.getInput();
+        console.log(input);
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
-        console.log('add Item button pressed')
     }
     document.querySelector('.add__btn').addEventListener("click",ctrlAddItem)
 
