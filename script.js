28d73d79bee6d3b88c0b969314d62c6251814be4
Sagar <sagar@Sagars-MacBook-Air-2.local>
2019-04-30 10:57:46 +0530

    dry principle - added ctrlAddItem function

--- script.js
+++ script.js
@@ -16,16 +16,18 @@ var uiController = (function(){
 })
 
 var controller = (function(budgetCtrl,UIctrl){
-    document.querySelector('.add__btn').addEventListener("click",function(){
+    var ctrlAddItem = function(){
         // 1. Get the field input data
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
-    })
+        console.log('add Item button pressed')
+    }
+    document.querySelector('.add__btn').addEventListener("click",ctrlAddItem)
 
     document.addEventListener('keypress',function(e){
-        if(e.keyCode == 13){
+        if(e.keyCode === 13 || event.which === 13){
             console.log('enter was pressed')
         }
     })
