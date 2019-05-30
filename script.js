135b02fadc9f3f5064d9484791f9a2461c8b0aff
mr-inu <35868217+mr-inu@users.noreply.github.com>
2019-05-30 20:15:14 +0530

    first

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+var budgetController = (function U(){
+    var total = 100000;
+    var add = function(a){
+        total = total + a ;
+        console.log(total);
+    }
+    var subtract = function(b){
+        total = total - b ;
+        console.log(total);
+    }
+    return {
+        Test: function(c){
+            console.log(add(c));
+        },
+        Test2: function(d){
+            console.log(subtract(d));
+        }
+         
+    }
+})();
 
+var UIController = (function budgetctrl(){
+var z = budgetctrl.Test;
+var x = budgetctrl.Test2
+return{
+    secondTest: function(){
+        console.log(z);
+    },
+    thirdTest: function(){
+        console.log(x);
+    }
+}
+}
+)(budgetController);
+UIController.secondTest(500);
+UIController.thirdTest(500);
