2969b3d9897b30bb20d232736a018e713fdfdd02
Sagar <sagar@Sagars-MacBook-Air-2.local>
2019-06-02 15:16:21 +0530

    setting up event handlers

--- script.js
+++ script.js
@@ -19,41 +19,40 @@ var uiController = (function(){
 
     return {
         getInput : function(){
-            var DOMstrings = {
-                inputType: '.add__type',
-                inputDes : '.add__description',
-                inputVal : '.add__value'
-            }
             return {
                 type : document.querySelector(DOMstrings.inputType).value,
                 description : document.querySelector(DOMstrings.inputDes).value,
                 value : document.querySelector(DOMstrings.inputVal).value
-            }
-            // DOM strings are not available yet to be used by the controller
-            // so we now create another private variable which expose the DOM strings to the controller module publicly while keeping the variables private
-
-            getDOMstrings = function(){
-                return {DOMstrings}
             }
         }
     }
     
 })();
 
-var controller = (function(budgetCtrl,UIctrl){
-    // once we added the publicly exposed method above we now reference the returned object to DOM
-    var DOM = UIctrl.getDOMstrings();
-    var ctrlAddItem = function(){
-        // 1. Get the field input data
+var appController = (function(budgetCtrl,UIctrl){
+        // once we added the publicly exposed method above we now reference the returned object to DOM
+        var DOM = UIctrl.getDOMstrings();
+        var ctrlAddItem = function(){
+
         var input = UIctrl.getInput();
         console.log(input);
+
+    }
+
+    // Fetching add button from the DOM and attaching an event listener to it
+    // TOY0001 - console lot message "button was pressed" when ever add button is pressed
+
+    document.querySelector('.add__btn').addEventListener("click",ctrlAddItem) 
+
+        // 1. Get the field input data
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
-    }
-    document.querySelector('.add__btn').addEventListener("click",ctrlAddItem)
 
+    // adding event listener to the keypress event => resource(https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode)
+    // TOY0002 - Find out the logic that will be replaced from the if logic below and what logic would be implemented.
+    // TOY0002 (cont) - Replace the console.log statement accordingly
     document.addEventListener('keypress',function(e){
         if(e.keyCode === 13 || event.which === 13){
             console.log('enter was pressed')
